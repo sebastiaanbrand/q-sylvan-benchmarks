ffbc20610d4d06b75df04897d14cf7d8bd2d564b
Sebastiaan Brand <sebastiaanbrand@gmail.com>
2023-09-01 12:14:40 +0200

    plot tool comparison

--- scripts/generate_plots.py
+++ scripts/generate_plots.py
@@ -3,12 +3,24 @@ import sys
 import json
 import argparse
 from typing import Iterable
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from pathlib import Path
+
+timeout_time = 600 # replaces NaN from timeout with this time in the plots
+formats = ['png']
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('folder')
+
+
+def plots_folder(args):
+    """
+    Folder to put the plots.
+    """
+    return os.path.join(args.folder, 'plots')
 
 
 def load_data(folder : str):
@@ -35,7 +47,74 @@ def load_data(folder : str):
                 new_df = pd.DataFrame(data, index=[0])
                 df = pd.concat([df, new_df])
 
-    return df[['benchmark', 'tool', 'simulation_time', 'max_nodes']]
+    return df[['benchmark', 'tool', 'simulation_time', 'workers', 'max_nodes']]
+
+
+def _plot_diagonal_lines(ax, min_val, max_val, at=[0.1, 10]):
+    """
+    Add diagonal lines to ax
+    """
+    
+    # bit of margin for vizualization
+    #ax.set_xlim([min_val-0.15*min_val, max_val+0.15*max_val])
+    #ax.set_ylim([min_val-0.15*min_val, max_val+0.15*max_val])
+
+    # diagonal lines
+    ax.plot([min_val, max_val], [min_val, max_val], ls="--", c="gray")
+    for k in at:
+        ax.plot([min_val, max_val], [min_val*k, max_val*k], ls=":", c="#767676")
+
+    return ax
+
+
+def plot_scatter(data_x, data_y, data_labels, label_x, label_y, filename, args):
+    """
+    Produce scatter plot of data_x vs data_y
+    """
+    fig, ax = plt.subplots()
+
+    col = 'royalblue'
+    fc_cols = np.array([col for _ in range(len(data_x))])
+    fc_cols[data_x == timeout_time] = 'none'
+    fc_cols[data_y == timeout_time] = 'none'
+
+    # plot the data + diagonal line
+    ax = _plot_diagonal_lines(ax, 0, timeout_time, at=[])
+    ax.scatter(data_x, data_y, facecolors=fc_cols, edgecolors=col)
+
+    # axis labels, etc.
+    ax.set_xlabel(label_x)
+    ax.set_ylabel(label_y)
+    
+    # save figure
+    outputpath = os.path.join(plots_folder(args), 'mqt_vs_qsylvan')
+    for _format in formats:
+        fig.savefig(f"{outputpath}.{_format}")
+    
+    # save version of the figure with labeled data points
+    for i, bench_name in enumerate(data_labels):
+        ax.annotate(bench_name, (data_x[i], data_y[i]), fontsize=1.0, rotation=60)
+    fig.savefig(f"{outputpath}_annotated.pdf")
+    fig.clf()
+
+
+def plot_tool_comparison(df : pd.DataFrame, args):
+    """
+    Plot Q-Sylvan vs MQT-DDSIM
+    """
+    left = df.loc[df['tool'] == 'mqt']
+    right = df.loc[df['tool'] == 'q-sylvan']
+
+    joined = pd.merge(left, right, on='benchmark', how='outer', suffixes=('_l','_r'))
+    print(joined)
+
+    data_l = joined['simulation_time_l'].fillna(timeout_time)
+    data_r = joined['simulation_time_r'].fillna(timeout_time)
+    data_labels = joined['benchmark']
+
+    plot_scatter(data_l, data_r, data_labels, 
+                'MQT-DDSIM time', 'Q-Sylvan time (s)',
+                'mqt_vs_qsylvan', args)
 
 
 def plot_speedups(df : pd.DataFrame, alg_selection : Iterable, output_file : str,
@@ -79,7 +158,6 @@ def plot_speedups(df : pd.DataFrame, alg_selection : Iterable, output_file : str
     fig.clf()
 
 
-
 def plot_speedups_selection(df : pd.DataFrame):
 
     # plot some selection on grover
@@ -97,5 +175,6 @@ def plot_speedups_selection(df : pd.DataFrame):
 if __name__ == '__main__':
     args = parser.parse_args()
     df = load_data(args.folder)
-    print(df)
-    #plot_speedups_selection(df)
+    
+    Path(plots_folder(args)).mkdir(parents=True, exist_ok=True)
+    plot_tool_comparison(df, args)
