13bf4916c635a3117f087e5b4eb140c21f0fbfd7
Sebastiaan Brand <sebastiaanbrand@gmail.com>
2024-06-04 12:04:35 +0200

    add option to exp gen to limit num qubits

--- scripts/generate_experiments.py
+++ scripts/generate_experiments.py
@@ -1,6 +1,8 @@
+"""
+Python script to generate bash scipts which run benchmarks.
+"""
 import re
 import os
-import sys
 import argparse
 from datetime import datetime
 from pathlib import Path
@@ -18,6 +20,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('qasm_dir', help="Path of directory with .qasm files.")
 parser.add_argument('--name', help="Name for experiments dir.")
 parser.add_argument('--log_vector', action='store_true', default=False, help="Log entire final state vector.")
+parser.add_argument('--max_qubits', type=int, help="Get all qasm files from qasm_dir, up to max_qubits.")
 parser.add_argument('--test_multicore', action='store_true', default=False, help="Run multicore benchmarks.")
 parser.add_argument('--test_norm_strats', action='store_true', default=False, help="Run with different norm strats.")
 parser.add_argument('--test_inv_caching', action='store_true', default=False ,help="Run with both INV-CACHING on/off.")
@@ -27,6 +30,9 @@ parser.add_argument('--recursive', action='store_true', default=False, help="Rec
 
 
 def init_output_file():
+    """
+    Initialize the output bash file.
+    """
     global output_file
     global csv_output
     dt_string = datetime.now().strftime("%Y%m%d%m_%H%M%S")
@@ -34,7 +40,7 @@ def init_output_file():
     csv_output  = csv_output.format(dt_string)
     print(f"writing to {output_file}")
 
-    with open(output_file, 'w') as f:
+    with open(output_file, 'w', encoding='utf-8') as f:
         f.write("#!/bin/bash\n\n")
 
 
@@ -50,6 +56,27 @@ def natural_sorting(filename : str):
     Used for sorting strings containing numbers in numerical way.
     """
     return [ atoi(c) for c in re.split(r'(\d+)', filename)]
+
+
+def get_num_qubits(filename : str):
+    """
+    Assumes first number in the filename is the number of qubits.
+    """
+    nums = re.findall(r'\d+', filename)
+    if len(nums) >= 1:
+        return int(nums[0])
+    return None
+
+
+def skip(filename : str, args):
+    """
+    Returns True if file should be skipped based on args.
+    """
+    if args.max_qubits is not None:
+        if get_num_qubits(filename) is None or\
+           get_num_qubits(filename) > args.max_qubits:
+            return True
+    return False
 
 
 def experiments_qasm(args):
@@ -78,9 +105,9 @@ def experiments_qasm(args):
     norm_strats = [' -s low', ' -s largest', ' -s l2'] if args.test_norm_strats else ['']
 
     print(f"Writing to {bash_file_all}, {bash_file_mqt}, {bash_file_qsy}")
-    with open(bash_file_all, 'w') as f_all,\
-         open(bash_file_mqt, 'w') as f_mqt,\
-         open(bash_file_qsy, 'w') as f_qsy:
+    with open(bash_file_all, 'w', encoding='utf-8') as f_all,\
+         open(bash_file_mqt, 'w', encoding='utf-8') as f_mqt,\
+         open(bash_file_qsy, 'w', encoding='utf-8') as f_qsy:
         f_all.write("#!/bin/bash\n\n# Q-Sylvan + MQT DDSIM benchmarks\n")
         f_mqt.write("#!/bin/bash\n\n# MQT DDSIM benchmarks\n")
         f_qsy.write("#!/bin/bash\n\n# Q-Sylvan benchmarks\n")
@@ -101,6 +128,8 @@ def experiments_qasm(args):
         exp_counter = 0
         for filepath in sorted(filepaths, key=natural_sorting):
             filename = os.path.basename(filepath)
+            if skip(filename, args):
+                continue
             # MQT
             json_output = f"{output_dir}/json/{filename[:-5]}_mqt.json"
             log         = f"{output_dir}/json/{filename[:-5]}_mqt.log"
