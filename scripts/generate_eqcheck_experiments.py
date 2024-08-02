e8ff73e9926a31ecdbaead6c70cc474e3650b80c
Sebastiaan Brand <sebastiaanbrand@gmail.com>
2024-08-02 15:19:29 +0200

    include shift4+shift7 in experiments

--- scripts/generate_eqcheck_experiments.py
+++ scripts/generate_eqcheck_experiments.py
@@ -20,6 +20,7 @@ parser.add_argument('qasm_dir', help="Path to dir with subdirs origin/, opt/ gm/
 parser.add_argument('--name', help="Name for experiments dir.")
 parser.add_argument('--norm_strat', choices=['low','max','min','l2'], default='max', help="Norm strat to use for all runs.")
 parser.add_argument('--test_multicore', action='store_true', default=False, help="Run multicore benchmarks.")
+parser.add_argument('--include_shift', action='store_true', default=False, help="Include shift 1e-4 and 1e-7 eqchecks.")
 parser.add_argument('--timeout', action='store', default=600, help='Timeout time per benchmark in seconds')
 
 
@@ -48,18 +49,25 @@ def experiments_eqcheck(args):
     Path(os.path.join(output_dir,'json')).mkdir(parents=True, exist_ok=True)
     Path(os.path.join(output_dir,'logs')).mkdir(parents=True, exist_ok=True)
     Path(os.path.join(output_dir,'meta')).mkdir(parents=True, exist_ok=True)
-    bash_file = os.path.join(output_dir, 'run_all.sh')
+    bash_file_all = os.path.join(output_dir, 'run_all.sh')
+    bash_file_qsy = os.path.join(output_dir, 'run_qsylvan.sh')
+    bash_file_quo = os.path.join(output_dir, 'run_quokkasharp.sh')
 
     cli_args = ''
     if args.test_multicore:
         cli_args += ' --count-nodes'
     workers = [1,2,4,8] if args.test_multicore else [1]
 
-    print(f"Writing to {bash_file}")
+    print(f"Writing to {bash_file_all}, {bash_file_qsy}, {bash_file_quo}")
     origin_dir = os.path.join(args.qasm_dir, 'origin')
-    with open(bash_file, 'w', encoding='utf-8') as f:
-        f.write("#!/bin/bash\n\n# Circuit equivalence checking benchmarks\n\n")
-        f.write(f"# Quokka-Sharp timeout\nexport TIMEOUT={args.timeout}\n")
+    with open(bash_file_all, 'w', encoding='utf-8') as f_all,\
+         open(bash_file_qsy, 'w', encoding='utf-8') as f_qsy,\
+         open(bash_file_quo, 'w', encoding='utf-8') as f_quo:
+        f_all.write("#!/bin/bash\n\n# Circuit equivalence checking benchmarks\n")
+        f_qsy.write("#!/bin/bash\n\n# Circuit equivalence checking benchmarks\n")
+        f_quo.write("#!/bin/bash\n\n# Circuit equivalence checking benchmarks\n")
+        f_all.write(f"\n# Quokka-Sharp timeout\nexport TIMEOUT={args.timeout}\n")
+        f_quo.write(f"\n# Quokka-Sharp timeout\nexport TIMEOUT={args.timeout}\n")
 
         # get qasm files
         origin_files = []
@@ -69,8 +77,13 @@ def experiments_eqcheck(args):
 
         # write to bash script
         exp_counter = 0
-        for comp_dir, comp_ext in [('opt','.opt.qasm'), ('gm','.gm.qasm'), ('flip', '.fp.qasm')]:
-            f.write(f'\n# {comp_dir}\n')
+        selection = [('opt','.opt.qasm'), ('gm','.gm.qasm'), ('flip', '.fp.qasm')]
+        if args.include_shift:
+            selection += [('shift4','.ps4.qasm'), ('shift7','.ps7.qasm')]
+        for comp_dir, comp_ext in selection:
+            f_all.write(f'\n# {comp_dir}\n')
+            f_qsy.write(f'\n# {comp_dir}\n')
+            f_quo.write(f'\n# {comp_dir}\n')
 
             comp_dir = os.path.join(args.qasm_dir, comp_dir)
             for origin_file in origin_files:
@@ -90,7 +103,8 @@ def experiments_eqcheck(args):
                     json_out = f"{output_dir}/json/{origin_file[:-5]}_quokkasharp_{exp_counter}.json"
                     log      = f"{output_dir}/logs/{origin_file[:-5]}_quokkasharp_{exp_counter}.log"
                     meta     = f"{output_dir}/meta/{origin_file[:-5]}_quokkasharp_{exp_counter}.json"
-                    f.write(QUOKKA_SHARP.format(origin_path, compare_path, w, log, json_out))
+                    f_all.write(QUOKKA_SHARP.format(origin_path, compare_path, w, log, json_out))
+                    f_quo.write(QUOKKA_SHARP.format(origin_path, compare_path, w, log, json_out))
                     with open(meta, 'w', encoding='utf-8') as meta_file:
                         json.dump({ 'circuit_type' : origin_file.split('_')[0],
                                     'circuit_U' : origin_file[:-5],
@@ -108,7 +122,8 @@ def experiments_eqcheck(args):
                     json_out = f"{output_dir}/json/{origin_file[:-5]}_qsylvan_{w}_{exp_counter}.json"
                     log      = f"{output_dir}/logs/{origin_file[:-5]}_qsylvan_{w}_{exp_counter}.log"
                     meta     = f"{output_dir}/meta/{origin_file[:-5]}_qsylvan_{w}_{exp_counter}.json"
-                    f.write(Q_SYLVAN.format(args.timeout, origin_path, compare_path, w, cli_args, log, json_out))
+                    f_all.write(Q_SYLVAN.format(args.timeout, origin_path, compare_path, w, cli_args, log, json_out))
+                    f_qsy.write(Q_SYLVAN.format(args.timeout, origin_path, compare_path, w, cli_args, log, json_out))
                     with open(meta, 'w', encoding='utf-8') as meta_file:
                         json.dump({ 'circuit_type' : origin_file.split('_')[0],
                                     'circuit_U' : origin_file[:-5],
