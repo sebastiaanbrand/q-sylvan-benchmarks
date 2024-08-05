979902839e2e359c1e044ddc1d005a3c45ba62b1
Sebastiaan Brand <sebastiaanbrand@gmail.com>
2024-08-05 17:00:52 +0200

    quasimodo dependencies

--- compile_sources.sh
+++ compile_sources.sh
@@ -33,4 +33,32 @@ cd tools/mqt-ddsim
 if [[ $recompile ]]; then rm -r -f build; fi
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release
-cd ../../..
+cd ../..
+
+# get Quasimode dependencies
+if [ ! -d tools/_deps/boost_1_81_0 ]; then
+  mkdir -p tools/_deps/
+  cd tools/_deps/
+  if [ ! -f boost_1_81_0.tar.gz ]; then
+    wget https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.gz
+  fi
+  tar -xf boost_1_81_0.tar.gz
+  cd ../..
+fi
+export BOOST_PATH="$PWD/tools/_deps/boost_1_81_0"
+export PYTHON_INCLUDE="$PWD/.venv/include"
+
+
+# compile Quasimodo
+cd tools/Quasimodo/cflobdd/cudd-complex-big/
+autoupdate
+autoreconf
+# "Edit configure to add -fPIC flag to CFLAGS and CXXFlAGS and then run the following after saving the file"
+# ^ ?
+./configure
+make
+cd ../..
+cd python_pkg/
+invoke build-quasimodo
+invoke build-pybind11
+cd ../../../
