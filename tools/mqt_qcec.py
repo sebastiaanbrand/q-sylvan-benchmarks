2e8e4cd624dd4a9c16bd959c60c11f94b57d744a
Sebastiaan Brand <sebastiaanbrand@gmail.com>
2024-09-26 10:48:21 +0200

    option to select specific alg from qcec

--- tools/mqt_qcec.py
+++ tools/mqt_qcec.py
@@ -11,14 +11,31 @@ from mqt import qcec
 parser = argparse.ArgumentParser()
 parser.add_argument('qasmfile1', help="Path to .qasm file.")
 parser.add_argument('qasmfile2', help="Path to .qasm file.")
+parser.add_argument('--alg', choices=['alt', 'con', 'sim', 'zx', 'all'], default='all')
 parser.add_argument('--workers', type=int, default=1, help="Number of parallel processes.")
+
+
+def set_algorithms(args):
+    """
+    Return tuple of Bools (run_alt, run_con, run_sim, run_zx)
+    """
+    return (args.alg in ('alt', 'all'),
+            args.alg in ('con', 'all'),
+            args.alg in ('sim', 'all'),
+            args.alg in ('zx',  'all'))
 
 
 def equivalence_check(args):
     """
     Run circuit equivalence checking using MQT QCEC on given circuits.
     """
-    res = qcec.verify(args.qasmfile1, args.qasmfile2, nthreads=args.workers)
+    run_alt, run_con, run_sim, run_zx = set_algorithms(args)
+    res = qcec.verify(args.qasmfile1, args.qasmfile2,
+                      run_alternating_checker=run_alt,
+                      run_construction_checker=run_con,
+                      run_simulation_checker=run_sim,
+                      run_zx_checker=run_zx,
+                      nthreads=args.workers)
 
     # write stats as JSON
     stats = {}
